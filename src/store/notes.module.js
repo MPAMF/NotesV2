08065c7d45df1f08410e1b1a25bdb655ca3864fd
SchawnnDev <iperskill@gmail.com>
2021-10-14 18:14:17 +0200

    hotfix

--- src/store/notes.module.js
+++ src/store/notes.module.js
@@ -86,7 +86,7 @@ const actions = {
 
     createSession({commit}) {
         commit('startFetching')
-        return new Promise(((resolve, reject) => axios.post('sessions/', {notes: []}).then(({data}) => {
+        return new Promise(((resolve, reject) => axios.post('sessions/', {notes: [], selected_tp: null}).then(({data}) => {
             commit('setSessionId', data['session_key'])
             commit('setNotesLoaded', true)
             resolve()
